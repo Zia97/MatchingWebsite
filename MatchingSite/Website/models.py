dc7ee87702ff6077cbdfd607b02d0e9052bbcb9f
Qasim Ziauddin <q.h.ziauddin@se15.qmul.ac.uk>
2018-11-21 14:48:23 +0000

    db changes

--- MatchingSite/Website/models.py
+++ MatchingSite/Website/models.py
@@ -4,7 +4,7 @@ class UserProfile(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     password = models.CharField(max_length=200)
-    #profile_image = models.ImageField(upload_to="PicFolder/",default="PicFolder/ball.jpg")
+    profile_image = models.ImageField(upload_to="PicFolder/",default="PicFolder/ball.jpg")
     email = models.EmailField(max_length=254)
     gender = models.CharField(max_length=1)
     #dob = models.DateField(auto_now=False, auto_now_add=False)
@@ -13,3 +13,4 @@ class UserProfile(models.Model):
 class Hobbies(models.Model):
     #question = models.ForeignKey(Question, on_delete=models.CASCADE)
     hobby = models.CharField(max_length=200)
+    userProf = models.ManyToManyField(UserProfile);
