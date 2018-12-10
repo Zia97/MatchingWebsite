06ed19bb67d671818e9acd90d5bccd79a2df7277
Qasim Ziauddin <q.h.ziauddin@se15.qmul.ac.uk>
2018-12-10 17:25:17 +0000

    models and db

--- MatchingSite/Website/models.py
+++ MatchingSite/Website/models.py
@@ -1,17 +1,15 @@
 from django.db import models
+import datetime
 
 class UserProfile(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
-    username = models.CharField(max_length=200)
-    password = models.CharField(max_length=200)
-    #profile_image = models.ImageField(upload_to="PicFolder/",default="PicFolder/ball.jpg")
+    username = models.CharField(max_length=20)
     email = models.EmailField(max_length=254)
     gender = models.CharField(max_length=1)
-    #dob = models.DateField(auto_now=False, auto_now_add=False)
-    #listOfHobbies =  a
+    dob = models.DateField()
+    hobUser = models.ManyToManyField('Hobbies')
 
 class Hobbies(models.Model):
-    #question = models.ForeignKey(Question, on_delete=models.CASCADE)
     hobby = models.CharField(max_length=200)
-    userProf = models.ManyToManyField(UserProfile);
+    userProf = models.ManyToManyField('UserProfile')
